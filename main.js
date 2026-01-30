02c7d7671375904d440a88bbbc80c329aabb24d9
nghiale <nghiale132004@gmail.com>
2026-01-30 13:45:41 +0700

    remove log

--- main.js
+++ main.js
@@ -3,13 +3,5 @@ import { Game } from "./app.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     const decodedData = decodeData();
-
-    if (!decodedData || !decodedData.data) {
-        console.error("❌ Decode data FAILED", decodedData);
-        return;
-    }
-
-    console.log("✅ Decode data OK", decodedData);
-
     window.game = new Game(decodedData);
 });
